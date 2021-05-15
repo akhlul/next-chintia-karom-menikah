7f53ced5daa1c9fe7b740f5735795c82fe47ba2e
Ahlul Karom <karom@bps.go.id>
2021-05-15 23:38:15 +0700

    fix font with header

---
+++ pages/_document.js
@@ -0,0 +1,41 @@
+import Document, {
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+
+    return initialProps
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Nunito+Sans&display=swap"
+            media="print" onLoad="this.media='all'"
+          />
+          <link
+            rel="preload"
+            href="/fonts/HeartBreath-Demo.ttf"
+            as="font"
+            crossOrigin=""
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
