fddf2218c63f18b87b5a2066b8b688f7371951a7
Ahlul Karom <karom@bps.go.id>
2021-05-15 23:20:16 +0700

    attach font

--- components/landing.js
+++ components/landing.js
@@ -182,7 +182,7 @@ const Landing = () => {
             <p className="lg:w-2/3 mx-auto font-sans font-thin">Desa Plandi Utara RT 21/RW 05 Kec. Jombang, Kab. Jombang</p>
           </div>
           <div className="flex flex-col text-center mt-16 mx-auto">
-            <button className="mx-auto text-white bg-brand-500 rounded-3xl border-0 py-1 px-8 focus:outline-none hover:bg-brand-600 rounded text-lg font-sans">Buka Undangan</button>
+            <button className="mx-auto text-white bg-brand-500 rounded-3xl border-0 py-1 px-8 focus:outline-none hover:bg-brand-600 rounded text-lg font-sans">Menuju Lokasi</button>
           </div>
         </div>
       </div>
@@ -192,9 +192,9 @@ const Landing = () => {
       <div className="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
         <div className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
           {/* style={{ filter: 'grayscale(1) contrast(1.2) opacity(0.4)' }} */}
-          <iframe className="absolute inset-0" scrolling="no" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d988.7853547030128!2d112.24348062917821!3d-7.559555599659259!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zN8KwMzMnMzQuNCJTIDExMsKwMTQnMzguNSJF!5e0!3m2!1sid!2sid!4v1621077842151!5m2!1sid!2sid&amp;width=100%&amp;height=600&amp;hl=en&amp;q=%C4%B0zmir+(My%20Business%20Name)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed" width="100%" height="100%" frameborder="0"></iframe>
+          <iframe className="absolute inset-0" scrolling="no" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d988.7853547030128!2d112.24348062917821!3d-7.559555599659259!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zN8KwMzMnMzQuNCJTIDExMsKwMTQnMzguNSJF!5e0!3m2!1sid!2sid!4v1621077842151!5m2!1sid!2sid&amp;width=100%&amp;height=600&amp;hl=en&amp;q=%C4%B0zmir+(My%20Business%20Name)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed" width="100%" height="100%" frameBorder="0"></iframe>
           {/* <iframe class="absolute inset-0" scrolling="no" src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;q=%C4%B0zmir+(My%20Business%20Name)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed" width="100%" height="100%" frameborder="0"></iframe> */}
-          <div className="bg-white relative flex flex-wrap py-6 rounded shadow-md">
+          {/* <div className="bg-white relative flex flex-wrap py-6 rounded shadow-md">
             <div className="lg:w-1/2 px-6">
               <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">ADDRESS</h2>
               <p className="mt-1">Photo booth tattooed prism, portland taiyaki hoodie neutra typewriter</p>
@@ -205,21 +205,21 @@ const Landing = () => {
               <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">PHONE</h2>
               <p className="leading-relaxed">123-456-7890</p>
             </div>
-          </div>
+          </div> */}
         </div>
         <div className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
           <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">Feedback</h2>
           <p className="leading-relaxed mb-5 text-gray-600">Post-ironic portland shabby chic echo park, banjo fashion axe</p>
           <div className="relative mb-4">
-            <label for="name" className="leading-7 text-sm text-gray-600">Name</label>
+            <label for_="name" className="leading-7 text-sm text-gray-600">Name</label>
             <input type="text" id="name" name="name" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
       </div>
             <div className="relative mb-4">
-              <label for="email" className="leading-7 text-sm text-gray-600">Email</label>
+              <label for_="email" className="leading-7 text-sm text-gray-600">Email</label>
               <input type="email" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
       </div>
               <div className="relative mb-4">
-                <label for="message" className="leading-7 text-sm text-gray-600">Message</label>
+                <label for_="message" className="leading-7 text-sm text-gray-600">Message</label>
                 <textarea id="message" name="message" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
               </div>
               <button className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
