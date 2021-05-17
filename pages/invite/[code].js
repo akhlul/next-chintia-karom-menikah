672597627d58b9654897a34ab0bbcfcb36a8a2b1
Ahlul Karom <karom@bps.go.id>
2021-05-17 08:57:45 +0700

    change into initialprop

--- pages/invite/[code].js
+++ pages/invite/[code].js
@@ -9,43 +9,43 @@ import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 // Component
 import Landing2 from '../../components/landing2'
 
-export async function getStaticPaths() {
-  return { paths: [], fallback: false };
-}
+// export async function getStaticPaths() {
+//   return { paths: [], fallback: false };
+// }
 
-export async function getStaticProps({ params }) {
-  const { code } = params;
-  console.log(code)
+// export async function getStaticProps({ params }) {
+//   const { code } = params;
+//   console.log(code)
 
-  try {
-    const tamu = await fetchTamu(code);
-    console.log(tamu)
+//   try {
+//     const tamu = await fetchTamu(code);
+//     console.log(tamu)
 
-    // if (!tamu) {
-    //   return {
-    //     redirect: {
-    //       destination: '/',
-    //       permanent: false,
-    //     },
-    //   }
-    // }
-    // return { props: { tamu } }
+//     // if (!tamu) {
+//     //   return {
+//     //     redirect: {
+//     //       destination: '/',
+//     //       permanent: false,
+//     //     },
+//     //   }
+//     // }
+//     // return { props: { tamu } }
 
-    // test only
-    return tamu ? { props: { tamu } } : { notFound: true };
-  } catch (error) {
-    console.error(error);
-    // return {
-    //   redirect: {
-    //     destination: '/',
-    //     permanent: false,
-    //   },
-    // }
+//     // test only
+//     return tamu ? { props: { tamu } } : { notFound: true };
+//   } catch (error) {
+//     console.error(error);
+//     // return {
+//     //   redirect: {
+//     //     destination: '/',
+//     //     permanent: false,
+//     //   },
+//     // }
 
-    // test only
-    return { notFound: true }
-  }
-}
+//     // test only
+//     return { notFound: true }
+//   }
+// }
 
 const fetchTamu = async (code) => {
   let { data, error } = await supabase
@@ -61,26 +61,63 @@ const fetchTamu = async (code) => {
 const Invites = ({ tamu }) => {
   const router = useRouter()
 
-  // If the page is not yet generated, this will be displayed
-  // initially until getStaticProps() finishes running
-  if (router.isFallback) {
-    return (
-      <Loader
-        className="absolute inset-0 z-100 flex items-center justify-center bg-brand-500"
-        visible={true}
-        type="Puff"
-        color="#FFF"
-        height={100}
-        width={100}
-      // timeout={3000} //3 secs
-      />
-    )
-    // return <div>Loading...</div>
-  }
+  // // If the page is not yet generated, this will be displayed
+  // // initially until getStaticProps() finishes running
+  // if (router.isFallback) {
+  //   return (
+  //     <Loader
+  //       className="absolute inset-0 z-100 flex items-center justify-center bg-brand-500"
+  //       visible={true}
+  //       type="Puff"
+  //       color="#FFF"
+  //       height={100}
+  //       width={100}
+  //     // timeout={3000} //3 secs
+  //     />
+  //   )
+  //   // return <div>Loading...</div>
+  // }
 
   return (
-    <Landing2 tamu={ tamu }/>
+    <Landing2 tamu={tamu}/>
   )
+}
+
+Invites.getInitialProps = async (ctx) => {
+  // const res = await fetch('https://api.github.com/repos/vercel/next.js')
+  // const json = await res.json()
+  // return { stars: json.stargazers_count }
+
+  try {
+    const { code } = ctx.query;
+    console.log(ctx.query)
+    const tamu = await fetchTamu(code);
+    console.log(tamu)
+
+    // if (!tamu) {
+    //   return {
+    //     redirect: {
+    //       destination: '/',
+    //       permanent: false,
+    //     },
+    //   }
+    // }
+    // return { props: { tamu } }
+
+    // test only
+    return tamu ? { tamu } : { notFound: true };
+  } catch (error) {
+    console.error(error);
+    // return {
+    //   redirect: {
+    //     destination: '/',
+    //     permanent: false,
+    //   },
+    // }
+
+    // test only
+    return { notFound: true }
+  }
 }
 
 export default Invites
